1f60287c25136a6a9c9a86be45f11fb7fa3e7f2b
Aston <aston.lviv@gmail.com>
2025-01-11 18:46:58 +0200

    initial commit

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1311690.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 65387860, 65387861, 65387862, 65387863, 65423939, 65426900, 65427681, 65427738, 65427740, 65433372, 65436744, 65439048, 65439050, 65440984, 66572386, 66574622, 67206604, 67348354, 67348818, 67349131, 67349138, 67349158 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/65387884/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/65387885/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/65387883/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/67344285/1/basis.wasm.js', 'wasmUrl' : 'files/assets/67344286/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/67344284/1/basis.js', 'preload' : false},
+];
